071e66429d92e50da6d65a0821d8d3abd6f5fad7
HPieniazek <pieniazekh@gmail.com>
2022-10-23 16:47:04 +0200

    cw3

--- cw3/src/main.rs
+++ cw3/src/main.rs
@@ -1,4 +1,5 @@
-
+extern crate numerals;
+use numerals::roman::Roman;
 
 fn main() {
 /*
@@ -6,12 +7,12 @@ fn main() {
  Wynik zapisz do zmienej romanNumber.
  Jeśli decimalNumber nie mieści sie w zakresie to umieść w romanNumber komunikat: Decimal number is out of range!
  Jeśli decimalNumber jest równe undefined lub null to umieść w romanNumber komunikat: Decimal number is undefined or null!
-*/use numerals::roman::Roman;
-    Roman::new(1).to_string();
+*/
     let decimal_number = 5;
-    let roman_number = Roman::from(decimal_number);
-    println!("{} = {}", decimal_number, roman_number);
+    use numerals::roman::Roman;
+    use numerals::roman::Numeral::{I, V, X};
+    let input    = Roman::from(decimal_number);
+    let expected = Roman::from(vec![ X, X, V, I, I ]);
+    assert_eq!(expected, input);
 }
-
-
 
