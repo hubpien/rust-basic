174b5b3fb25f2383c760677e3596ddecdc5a8c98
HPieniazek <pieniazekh@gmail.com>
2022-10-23 15:17:29 +0200

    cw1

--- cw1/src/main.rs
+++ cw1/src/main.rs
@@ -1,4 +1,13 @@
+/**
+* Napisz skrypt, który oblicza promień koła na podstawie pola powierzchni w zmiennej area
+* Wynik zapisz do zmiennej łańuchowej radius z dwoma miejscami po przecinku.
+*/
 fn main() {
-    let name = "World";
-    println!("My name is {}", name);
+    let area: f32 = 1.0;
+    let radius: f32;
+    let pi: f32 = 3.14;
+    radius = (area / pi) as f32;
+    println!("Promień koła wynosi: {}", radius);
 }
+
+
